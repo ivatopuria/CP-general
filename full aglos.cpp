6b05b02c181a2857da5939d674fdc96b3c6db8a8
ivatopuria <56364728+ivatopuria@users.noreply.github.com>
2020-04-24 21:48:18 +0400

    Update full aglos.cpp

--- full aglos.cpp
+++ full aglos.cpp
@@ -29,7 +29,51 @@
 #define ll long long
 #define fcin ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
 using namespace std;
+////// GCD
 
+int gcd(int x , int y){       
+  if(x==0) return y;
+  if(x>y) swap(x,y);
+  return gcd(y%x , x);
+}
+
+////////sieve of erathostenes
+  int p[N];
+  for(int i = 2 ; i*i < 10e5 ; i++){
+     for(int j = i ; j * i <= 10e5 ; j++){
+        p[j*i] = 1;
+     }
+  }
+////////fenwick
+      int tree[N];
+      
+      void update(int idx, int val) {
+        while (idx <= MaxIdx) {
+          tree[idx] += val;
+          idx += (idx & -idx);
+        }
+      }
+      int read(int idx) {
+        int sum = 0;
+        while (idx > 0) {
+          sum += tree[idx];
+          idx -= (idx & -idx);
+        }
+        return sum;
+      }
+/////////binary power
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+//
 int main(){
 fcin;
 
