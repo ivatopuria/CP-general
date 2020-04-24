4a9f8c8c53b9104af7ecf8b79fba92329494f12c
ivatopuria <56364728+ivatopuria@users.noreply.github.com>
2020-04-24 21:40:17 +0400

    Create full aglos.cpp

---
+++ full aglos.cpp
@@ -0,0 +1,36 @@
+//#include <fsociety>
+#include <cmath>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+//end of libraries ;
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+// ll lmax(ll o , ll w) {if(o>w)return o; return w;}
+// ll lmin(ll o , ll w) {if(o<w)return o; return w;}
+#define INF 3999999999999999999
+#define N 3000003
+#define PI 3.14159265359
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define fcin ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+using namespace std;
+
+int main(){
+fcin;
+
+}
