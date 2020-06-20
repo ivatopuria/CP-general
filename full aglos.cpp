63a478e54dc2fb397516f578df8e86bd4d4eb698
ivatopuria <56364728+ivatopuria@users.noreply.github.com>
2020-06-20 12:05:55 +0400

    Update full aglos.cpp

--- full aglos.cpp
+++ full aglos.cpp
@@ -145,8 +145,7 @@ for(int i = 1 ; i <= n ; i++){
 
 
 
-////
-Dijkstra
+////Dijkstra
 
 vector<int> ans;
 set < pair <int,int > > st;
@@ -182,6 +181,41 @@ else{
     for(auto each : ans) cout << each << " ";
 }
 
+////segment tree
+
+int t[N];
+void build(int v , int  tl , int  tr){
+    if(tl==tr){
+        t[v] = a[tl];
+    }
+    else{
+        int mid = (tl+tr)/2;
+        build(v*2 , tl , mid);
+        build(v*2 + 1 , mid + 1 , tr);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+int sum(int v , int tl , int tr , int  l , int r){
+    if(l>r) return 0;
+    if(l == tl && r == tr){
+        return t[v];
+    }
+    int mid = (tl+tr)/2;
+    return sum(v*2 , tl , mid , l , min( r , mid ) ) 
+          + sum(v*2 + 1 , mid + 1 , tr , max( l , mid + 1 ) , r);
+}
+void update(int v, int tl, int tr, int pos, int new_val) {
+    if (tl == tr) {
+        t[v] = new_val;
+    } else {
+        int mid = (tl + tr) / 2;
+        if (pos <= mid)
+            update(v*2, tl, mid, pos, new_val);
+        else
+            update(v*2+1, mid+ 1, tr, pos, new_val);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
 
 
 
